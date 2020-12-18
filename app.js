d2028856d1c0bed690776ab17b7a6d7fa87f6671
mini._.mali <frantisek.rapcak@gmail.com>
2020-12-18 11:56:46 +0100

    basic structure

---
+++ app.js
@@ -0,0 +1,32 @@
+//
+//  CONSTANTS
+//
+//const CONSTANT = 1.618;
+
+
+//
+//  variables
+//
+let 
+    give = document.querySelector('.plus'),
+    steal = document.querySelector('.minus'),
+    cookieStatusContainer = document.querySelector('#cookie-status'),
+    cookieStatus = cookieStatusContainer.textContent;
+
+
+
+
+console.log(cookieStatus);
+
+give.onclick = () => console.log('GIVE');
+steal.onclick = () => console.log('STEAL');
+
+
+
+
+
+
+
+
+
+
